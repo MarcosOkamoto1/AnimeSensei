2cd99e9d69224ffd3ed3dbd65b7e2c1a9c060e45
MarcosOkamoto1 <marcosviniciusokamoto2003@outlook.com>
2026-07-28 13:23:11 -0400

    Initial Commit

---
+++ AnimeSensei/AnimeSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeSenseiApp.swift
+//  AnimeSensei
+//
+//  Created by Marcos Vinicius Alencar de Souza Okamoto on 28/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimeSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeSensei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimeSensei
+//
+//  Created by Marcos Vinicius Alencar de Souza Okamoto on 28/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
